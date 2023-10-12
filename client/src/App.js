f1c53b7da0326985b6e533e254265e64b5505a4d
Narges <nargesmanafii@gmail.com>
2023-10-12 18:06:58 +0330

    ref: install react-router-dom and add login form in project

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from './component/Login';
 import React from "react";
 import SignUp from "./component/SignUp";
 
 const App = () => {
     return (
         <div>
-            <SignUp/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SignUp/>} />
+                    <Route path="/login" element={<Login/>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -2,35 +2,27 @@ import "../styles/SignUp.css"
 
 import React , {useState} from 'react';
 
-import { Link } from "react-router-dom";
 import Logo from "../assets/icons/Logo.png";
 import envelope from "../assets/icons/envelope-solid-48.png"
 import lock from "../assets/icons/lock-alt-solid-48 (2).png"
-import userCircle from "../assets/icons/user-circle-solid-48.png"
 
-const SignUp = () => {
+const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
   return (
     <div className="App">
       <div className="login-form">
         <img className="login-form__card-image" src={Logo} alt="logo" />
         <div className="login-form__header-image"></div>
-        <h2 className="login-form__title">عضویت</h2>
         <p className="login-form__subtitle">سلام!</p>
         <p className="login-form__description">لطفا مشخصات کاربری خود را در فرم زیر وارد کنید</p>
         <form>
           <div className="login-form__input-container">
             <img src={envelope} alt="envelope" className="login-form__icon" />
             <input type="email" placeholder="ایمیل" className="login-form__input"/>
-          </div>
-          <div className="login-form__input-container">
-            <img src={userCircle} alt="userCircle" className="login-form__icon" />
-            <input type="text" placeholder="نام کاربری" className="login-form__input"/>
           </div>
           <div className="login-form__input-container">
             <img src={lock} alt="lock" className="login-form__icon" />
@@ -45,12 +37,9 @@ const SignUp = () => {
           </div>
           <button type="submit" className="login-form__submit-button">ثبت نام</button>
         </form>
-        <p>
-          قبلاً ثبت نام کرده‌اید؟ <Link to="/login">وارد شوید</Link>
-        </p>
       </div>
     </div>
-  );
+  )
 }
 
-export default SignUp;
+export default Login;
